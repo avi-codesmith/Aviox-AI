c78647a76fce835858a6521f18878b9422f77edb
avi-codesmith <avi.codesmith@gmail.com>
2025-05-29 15:39:40 +0530

    Imlement the code

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 "use strict";
+
+const loader = document.querySelector(".loader");
+
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    loader.style.opacity = "0";
+  }, 1000);
+});
